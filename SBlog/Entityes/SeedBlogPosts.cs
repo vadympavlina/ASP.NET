d156df7ab6ff5b7c24c85ae9c9bc2a20ee79998b
Vadym Pavlina <vadim.table.pc@gmail.com>
2020-02-19 18:14:23 +0200

    Change seed

--- SBlog/Entityes/SeedBlogPosts.cs
+++ SBlog/Entityes/SeedBlogPosts.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using MVC_Intro.Models;
 using System.Linq;
-using System.Threading.Tasks;
-using MVC_Intro.Models;
 
 namespace MVC_Intro.Entityes
 {
@@ -15,55 +12,69 @@ namespace MVC_Intro.Entityes
                 context.Blog.AddRange(
                 new BlogModel
                 {
-                    author = "Neo",
-                    title = "RESUME VS COVER LETTER: MAIN DIFFERENCES BETWEEN",
-                    preview = "Do you know that a resume and a cover letter actually differ? Even though both of" +
-                " these documents are essential when applying for a job, it is critical that you know how to " +
-                "differentiate them in order to write them for the right purpose. Resume vs cover letter should" +
-                " be properly defined so that you",
-                    fullPost = "Do you know that a resume and a cover letter actually differ? Even though both of " +
-                "these documents are essential when applying for a job, it is critical that you know how to" +
-                " differentiate them in order to write them for the right purpose. Resume vs cover letter should" +
-                " be properly defined so that you understand what aim each of them has. When writing either a" +
-                " resume or a cover letter, you should demonstrate your skills and competencies in order to prove" +
-                " to the employer or HR manager that you are worth being considered for the position. Ideally," +
-                " both a resume and a cover letter should pinpoint that you have skills that are relevant and " +
-                "corresponding to the job opening. As you start working on the resume or a cover letter, " +
-                "be sure that you should come to realize the resume vs cover letter structure and mode or " +
-                "organization.Moreover,it is essential to adhere to the template and follow the general" +
-                " requirements as well as the resume cover letter format. First of all, a resume and a cover " +
-                "letter are not the same and each of them has a different structure and purpose.Second",
-                    img = "free-stock-photo-sites-2019-620x414.jpg"
+                    author = "Blizzard",
+                    title = "Overwatch PTR Patch Notes – February 13, 2020",
+                    preview = "Please note that the below patch notes only include changes currently available for testing on the PTR. While many of these changes may also be available on PlayStation 4 and Xbox One in a future patch, the PTR is PC-only and only reflects changes coming specifically to that platform.",
+                    fullPost = "BUG FIXES" +
+                    "Workshop" +
+                    "Fixed a bug where disabling certain flow control actions(If, Else If, Else, etc.) could lead to incorrect execution and incorrect editor indention" +
+                    "Fixed a bug where an Else If block followed by an End would cause a rule to prematurely abort if the Else If condition were false" +
+                    "Fixed a bug where the Loop If Condition and Abort If Condition actions considered the value of the conditions from the beginning of the frame instead of their current value" +
+                    "Fixed a bug where the Chase Global Variable actions did not have access to contextual values(such as Event Player, Attacker, Victim, etc.)" +
+                    "Fixed a bug where modifying a variable after chasing it would give the variable an incorrect value" +
+                    "Fixed a bug where modifying a single player variable after setting or modifying it as part of a multi - player variable change would give the variable an incorrect value" +
+                    "Fixed a bug where effects and beam effects that don’t reevaluate would wait until the end of the frame to evaluate their positions and radii rather than evaluating them immediately" +
+                    "Fixed a bug where the Total Elapsed Time value would be incorrect if used in a HUD action or in certain reevaluating actions" +
+                    "Fixed a bug where the Nearest Walkable Position value would be incorrect if used in a HUD action or in certain reevaluating actions" +
+                    "Fixed a bug where the Nearest Walkable Position value stopped working near the statue in Château Guillard" +
+                    "Fixed a bug where using the same Set Facing action multiple times in the same frame with different inputs would not work correctly" +
+                    "Fixed an editor crash caused by pasting subroutines into a slot outside of the default range" +
+                    "Fixed a bug where subroutines would stop executing upon encountering a disabled action",
+                    img = "09509d51-5837-48e6-b4b7-161c2ce8bbb0_overwatch_patch_notes_0-590x334.jpg"
                 },
              new BlogModel
              {
-                 author = "Ktoto",
-                 title = "RESUME VS COVER LETTER: MAIN DIFFERENCES BETWEEN",
-                 preview = "Do you know that a resume and a cover letter actually differ? Even though both of" +
-                " these documents are essential when applying for a job, it is critical that you know how to " +
-                " be properly defined so that you",
-                 fullPost = "Do you know that a resume and a cover letter actually differ? Even though both of " +
-                " to the employer or HR manager that you are worth being considered for the position. Ideally," +
-                " both a resume and a cover letter should pinpoint that you have skills that are relevant and " +
-                "corresponding to the job opening. As you start working on the resume or a cover letter, " +
-                "be sure that you should come to realize the resume vs cover letter structure and mode or " +
-                "organization.Moreover,it is essential to adhere to the template and follow the general" +
-                " requirements as well as the resume cover letter format. First of all, a resume and a cover " +
-                "letter are not the same and each of them has a different structure and purpose.Second",
-                 img = "from-the-hip-photo-breck.jpg"
+                 author = "Blizzard",
+                 title = "Overwatch PTR Patch Notes – February 7, 2020",
+                 preview = "Please note that the below patch notes only include changes currently available for testing on the PTR. While many of these changes may also be available on PlayStation 4 and Xbox One in a future patch, the PTR is PC-only and only reflects changes coming specifically to that platform.",
+                 fullPost = "BUG FIXES" +
+                    "Workshop" +
+                    "Fixed a bug where disabling certain flow control actions(If, Else If, Else, etc.) could lead to incorrect execution and incorrect editor indention" +
+                    "Fixed a bug where an Else If block followed by an End would cause a rule to prematurely abort if the Else If condition were false" +
+                    "Fixed a bug where the Loop If Condition and Abort If Condition actions considered the value of the conditions from the beginning of the frame instead of their current value" +
+                    "Fixed a bug where the Chase Global Variable actions did not have access to contextual values(such as Event Player, Attacker, Victim, etc.)" +
+                    "Fixed a bug where modifying a variable after chasing it would give the variable an incorrect value" +
+                    "Fixed a bug where modifying a single player variable after setting or modifying it as part of a multi - player variable change would give the variable an incorrect value" +
+                    "Fixed a bug where effects and beam effects that don’t reevaluate would wait until the end of the frame to evaluate their positions and radii rather than evaluating them immediately" +
+                    "Fixed a bug where the Total Elapsed Time value would be incorrect if used in a HUD action or in certain reevaluating actions" +
+                    "Fixed a bug where the Nearest Walkable Position value would be incorrect if used in a HUD action or in certain reevaluating actions" +
+                    "Fixed a bug where the Nearest Walkable Position value stopped working near the statue in Château Guillard" +
+                    "Fixed a bug where using the same Set Facing action multiple times in the same frame with different inputs would not work correctly" +
+                    "Fixed an editor crash caused by pasting subroutines into a slot outside of the default range" +
+                    "Fixed a bug where subroutines would stop executing upon encountering a disabled action",
+                 img = "66e23524-a96f-462c-a8ec-221c09f35137_144139-ow-patchnote-eichenwalde-article_m-1.jpeg"
              },
              new BlogModel
              {
-                 author = "Ktoto",
-                 title = "RESUME VS COVER LETTER: MAIN DIFFERENCES BETWEEN",
-                 preview = "Do you know that a resume and a cover letter actually differ? Even though both of" +
-                " these documents are essential when applying for a job, it is critical that you know how to " +
-                " be properly defined so that you",
-                 fullPost = "Do you know that a resume and a cover letter actually differ? Even though both of " +
-                " to the employer or HR manager that you are worth being considered for the position. Ideally," +
-                " requirements as well as the resume cover letter format. First of all, a resume and a cover " +
-                "letter are not the same and each of them has a different structure and purpose.Second",
-                 img = "photo_effects_filters_app.jpg"
+                 author = "Blizzard",
+                 title = "Overwatch PTR Patch Notes – February 4, 2020",
+                 preview = "Please note that the below patch notes only include changes currently available for testing on the PTR. While many of these changes may also be available on PlayStation 4 and Xbox One in a future patch, the PTR is PC-only and only reflects changes coming specifically to that platform.",
+                 fullPost = "GENERAL UPDATES" +
+                    "Workshop" +
+                    "Fixed a bug where disabling certain flow control actions(If, Else If, Else, etc.) could lead to incorrect execution and incorrect editor indention" +
+                    "Fixed a bug where an Else If block followed by an End would cause a rule to prematurely abort if the Else If condition were false" +
+                    "Fixed a bug where the Loop If Condition and Abort If Condition actions considered the value of the conditions from the beginning of the frame instead of their current value" +
+                    "Fixed a bug where the Chase Global Variable actions did not have access to contextual values(such as Event Player, Attacker, Victim, etc.)" +
+                    "Fixed a bug where modifying a variable after chasing it would give the variable an incorrect value" +
+                    "Fixed a bug where modifying a single player variable after setting or modifying it as part of a multi - player variable change would give the variable an incorrect value" +
+                    "Fixed a bug where effects and beam effects that don’t reevaluate would wait until the end of the frame to evaluate their positions and radii rather than evaluating them immediately" +
+                    "Fixed a bug where the Total Elapsed Time value would be incorrect if used in a HUD action or in certain reevaluating actions" +
+                    "Fixed a bug where the Nearest Walkable Position value would be incorrect if used in a HUD action or in certain reevaluating actions" +
+                    "Fixed a bug where the Nearest Walkable Position value stopped working near the statue in Château Guillard" +
+                    "Fixed a bug where using the same Set Facing action multiple times in the same frame with different inputs would not work correctly" +
+                    "Fixed an editor crash caused by pasting subroutines into a slot outside of the default range" +
+                    "Fixed a bug where subroutines would stop executing upon encountering a disabled action",
+                 img = "d6dc92a7-44a2-400f-861e-dbf4d959d556_overwatch-updates.jpg"
              }
             );
             }
